c554447162b9d7dab31919db98e5b736f04bde62
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-03-28 00:47:15 +0530

    Update 08.js

--- part1/08.js
+++ part1/08.js
@@ -9,22 +9,22 @@
 // BigInt
 // Symbol
 
-// let age = 22; 
-// let firstName = "harshit";
+let age = 22; 
+let firstName = "harshit";
 
-// // console.log(typeof age);
+console.log(typeof age);
 
 // // 22 -> "22"
 // // convert number to string. 
-// age = age + "";
-// console.log(typeof(age)); "22"
+age = age + "";
+console.log(typeof(age)); "22"
 
 
 // // convert string to number. 
 
-// let myStr = +"34";
-// console.log(typeof myStr);
+let myStr = +"34";
+console.log(typeof myStr);
 
-// let age = "18";
-// age = Number(age);
-// console.log(typeof age);
+let age = "18";
+age = Number(age);
+console.log(typeof age);
