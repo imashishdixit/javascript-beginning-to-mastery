09422c9f9f222cda6edafea0fbbee6106f1c1461
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-03-28 01:02:32 +0530

    Update 11.js

--- part1/11.js
+++ part1/11.js
@@ -1,21 +1,26 @@
 // undefined 
 // null
 
+/*
+-> varibale defined by const has to be intialized at the time of definition
+->
+*/
 let firstName;
-console.log(typeof firstName);
+console.log(typeof firstName); //undefined
 firstName = "Harshit";
-console.log(typeof firstName, firstName);
+console.log(typeof firstName, firstName); // string harshit
 
 let myVariable = null;
-console.log(myVariable);
+console.log(myVariable); // null
 myVariable = "harshit";
-console.log(myVariable, typeof myVariable);
-console.log(typeof null);
+console.log(myVariable, typeof myVariable); harshit string
+console.log(typeof null); //object
+
 // bug , error 
 
 // BigInt
-let myNumber = BigInt(12);
-let sameMyNumber = 123n;
-// console.log(myNumber);
-// console.log(Number.MAX_SAFE_INTEGER);
+let myNumber = BigInt(12); 
+let sameMyNumber = 123n; //big int
+console.log(myNumber);
+console.log(Number.MAX_SAFE_INTEGER);
 console.log(myNumber+ sameMyNumber);
