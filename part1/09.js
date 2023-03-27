e5cd5c8dbace125c441e5c0e0c4068488aaa307c
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-03-28 00:50:42 +0530

    Update 09.js

--- part1/09.js
+++ part1/09.js
@@ -1,7 +1,9 @@
 // string concatenation 
 
+
+//if we add + before string, it is converted into number which can be added
 let string1 = "17";
 let string2 = "10";
 
-let newString = +string1 + +string2;
+let newString = +string1 + +string2; //
 console.log(typeof newString);
