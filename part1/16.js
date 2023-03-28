ba14383adb0564c5c0f31d98c7d09be134e14c79
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-03-28 21:59:58 +0530

    Update 16.js

--- part1/16.js
+++ part1/16.js
@@ -2,19 +2,19 @@
 
 
 
-// if(firstName[0] === "H"){
-//     console.log("your name starts with H")
-// }
+if(firstName[0] === "H"){
+    console.log("your name starts with H")
+}
 
-// if(age > 18){
-//     console.log("you are above 18");
-// }
+if(age > 18){
+    console.log("you are above 18");
+}
 
-// if(firstName[0] === "H" && age>18){
-//     console.log("Name starts with H and above 18");
-// }else{
-//     console.log("inside else");
-// }
+if(firstName[0] === "H" && age>18){
+    console.log("Name starts with H and above 18");
+}else{
+    console.log("inside else");
+}
 let firstName = "arshit";
 let age = 16;
 
