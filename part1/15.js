854d335225441360b675034d327cd456649222a1
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-03-28 21:59:11 +0530

    Update 15.js

--- part1/15.js
+++ part1/15.js
@@ -1,18 +1,18 @@
 // ternary operator 
 
-// let age = 4;
-// let drink;
+let age = 4;
+let drink;
 
-// if(age>=5){
-//     drink = "coffee";
-// }else{
-//     drink = "milk";
-// }
+if(age>=5){
+    drink = "coffee";
+}else{
+    drink = "milk";
+}
 
-// console.log(drink);
+console.log(drink);
 
 // ternary operator / conditional operator 
 
-// let age = 3;
-// let drink = age >= 5 ? "coffee" : "milk";
-// console.log(drink);
+let age = 3;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
