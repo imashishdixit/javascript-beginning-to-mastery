7638fc168e81f193d23b14950761784eb7ad8d8f
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 12:24:39 +0530

    Update 38.js

--- part1/38.js
+++ part1/38.js
@@ -8,15 +8,15 @@ const person = {
 // for in loop 
 // Object.keys 
 
-// for(let key in person){
-//     // console.log(`${key} : ${person[key]}`);
-//     console.log(key," : " ,person[key]);
-// }
+for(let key in person){
+    // console.log(`${key} : ${person[key]}`);
+    console.log(key," : " ,person[key]);
+}
 
-// console.log(typeof (Object.keys(person)));
-// const val = Array.isArray((Object.keys(person)));
-// console.log(val);
+console.log(typeof (Object.keys(person)));
+const val = Array.isArray((Object.keys(person)));
+console.log(val);
 
-// for(let key of Object.keys(person)){
-//     console.log(person[key]);
-// }
+for(let key of Object.keys(person)){
+    console.log(person[key]);
+}
