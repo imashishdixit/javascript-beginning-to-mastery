0eebcbbcc20dde3c10296477542b1e50e809e073
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 01:28:13 +0530

    Update 68.js

--- part1/68.js
+++ part1/68.js
@@ -18,34 +18,34 @@
 // object literal 
 // key -> string 
 // key -> symbol
-// const person = {
-//     firstName : "harshit",
-//     age: 7,
-//     1:"one"
-// }
-// // console.log(person.firstName);
-// // console.log(person["firstName"]);
-// // console.log(person[1]);
-// for(let key in person){
-//     console.log(typeof key);
-// }
+const person = {
+    firstName : "harshit",
+    age: 7,
+    1:"one"
+}
+console.log(person.firstName);
+console.log(person["firstName"]);
+console.log(person[1]);
+for(let key in person){
+    console.log(typeof key);
+}
 
 // key value pair 
-// const person = new Map();
-// person.set('firstName', 'Harshit');
-// person.set('age', 7);
-// person.set(1,'one');
-// person.set([1,2,3],'onetwothree');
-// person.set({1: 'one'},'onetwothree');
-// console.log(person);
-// console.log(person.get(1));
-// for(let key of person.keys()){
-//     console.log(key, typeof key);
-// }
-// for(let [key, value] of person){
-//     // console.log(Array.isArray(key));
-//     console.log(key, value)
-// }
+const person = new Map();
+person.set('firstName', 'Harshit');
+person.set('age', 7);
+person.set(1,'one');
+person.set([1,2,3],'onetwothree');
+person.set({1: 'one'},'onetwothree');
+console.log(person);
+console.log(person.get(1));
+for(let key of person.keys()){
+    console.log(key, typeof key);
+}
+for(let [key, value] of person){
+    // console.log(Array.isArray(key));
+    console.log(key, value)
+}
 
 const person1 = {
     id: 1,
