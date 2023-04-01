95694e2d412a288046e21de49522c3cfe7f94682
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 16:07:33 +0530

    Update 59.js

--- part1/59.js
+++ part1/59.js
@@ -3,9 +3,9 @@ const numbers = [1,2,3,4,5, 10];
 
 // aim : sum of all the numbers in array 
 
-// const sum = numbers.reduce((accumulator, currentValue)=>{
-//     return accumulator + currentValue;
-// }, 100);
+const sum = numbers.reduce((accumulator, currentValue)=>{
+    return accumulator + currentValue;
+}, 100);
 
 // console.log(sum);
 // accumulator , currentValue,  return 
@@ -16,17 +16,17 @@ const numbers = [1,2,3,4,5, 10];
 // 15              10             25
 
 
-// const userCart = [
-//     {productId: 1, productName: "mobile", price: 12000},
-//     {productId: 2, productName: "laptop", price: 22000},
-//     {productId: 3, productName: "tv", price: 15000},
-// ]
+const userCart = [
+    {productId: 1, productName: "mobile", price: 12000},
+    {productId: 2, productName: "laptop", price: 22000},
+    {productId: 3, productName: "tv", price: 15000},
+]
 
-// const totalAmount = userCart.reduce((totalPrice, currentProduct)=>{
-//     return totalPrice + currentProduct.price;
-// }, 0)
+const totalAmount = userCart.reduce((totalPrice, currentProduct)=>{
+    return totalPrice + currentProduct.price;
+}, 0)
 
-// console.log(totalAmount);
+console.log(totalAmount);
 
 // total price      currentValue     return 
 // 0                {}                  12000
