59d308317c1d200ed2f67076106045e252689c36
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 12:45:04 +0530

    Update 40.js

--- part1/40.js
+++ part1/40.js
@@ -1,10 +1,10 @@
 // spread operator
-// const array1 = [1, 2, 3];
-// const array2 = [5, 6, 7];
+const array1 = [1, 2, 3];
+const array2 = [5, 6, 7];
 
-// // const newArray = [...array1, ...array2, 89, 69];
-// const newArray = [..."123456789"];
-// console.log(newArray);
+const newArray = [...array1, ...array2, 89, 69];
+const newArray = [..."123456789"];
+console.log(newArray);
 
 // spread operator in objects
 const obj1 = {
@@ -17,7 +17,7 @@ const obj2 = {
   key4: "value4",
 };
 
-// const newObject = { ...obj2, ...obj1, key69: "value69" };
-// const newObject = { ...["item1", "item2"] };
-// const newObject = { ..."abcdefghijklmnopqrstuvwxyz" };
-// console.log(newObject);
+const newObject = { ...obj2, ...obj1, key69: "value69" };
+const newObject = { ...["item1", "item2"] };
+const newObject = { ..."abcdefghijklmnopqrstuvwxyz" };
+console.log(newObject);
