624426bfe3244e7ff328019ab0e089233ec02369
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 15:49:10 +0530

    Update 56.js

--- part1/56.js
+++ part1/56.js
@@ -2,17 +2,17 @@
 
  const numbers = [4,2,5,8];
 
-// function myFunc(number, index){
-//     console.log(`index is ${index} number is ${number}`);
-// }
+function myFunc(number, index){
+    console.log(`index is ${index} number is ${number}`);
+}
 
-// numbers.forEach(function(number,index){
-//     console.log(`index is ${index} number is ${number}`);
-// });
+numbers.forEach(function(number,index){
+    console.log(`index is ${index} number is ${number}`);
+});
 
-// numbers.forEach(function(number, index){
-//     console.log(number*3, index);
-// })
+numbers.forEach(function(number, index){
+    console.log(number*3, index);
+})
 
 const users = [
     {firstName: "harshit", age: 23},
@@ -21,16 +21,16 @@ const users = [
     {firstName: "garima", age: 20},
 ]
 
-// users.forEach(function(user){
-//     console.log(user.firstName);
-// });
+users.forEach(function(user){
+    console.log(user.firstName);
+});
 
-// users.forEach((user, index)=>{
-//     console.log(user.firstName, index);
-// })
+users.forEach((user, index)=>{
+    console.log(user.firstName, index);
+})
 
-// for(let user of users){
-//     console.log(user.firstName);
-// }
+for(let user of users){
+    console.log(user.firstName);
+}
 
 
