feb30fd3a5ac4e23951a7b08178c3b238bf0671c
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 01:08:43 +0530

    Update 66.js

--- part1/66.js
+++ part1/66.js
@@ -2,21 +2,21 @@
 // jispe hum for of loop laga sakein
 // string , array are iterable 
 
-// const firstName = "Harshit";
-// for(let char of firstName){
-//     console.log(char);
-// }
+const firstName = "Harshit";
+for(let char of firstName){
+    console.log(char);
+}
 
 const items = ['item1', 'item2', 'item3'];
-// for(let item of items){
-//     console.log(item);
-// }
+for(let item of items){
+    console.log(item);
+}
 
 // array like object 
 // jinke pas length property hoti hai 
 // aur jiko hum index se access kar sakte hai
 // example :- string 
 
-// const firstName = "harshit";
-// console.log(firstName.length);
-// console.log(firstName[2]);
+const firstName = "harshit";
+console.log(firstName.length);
+console.log(firstName[2]);
