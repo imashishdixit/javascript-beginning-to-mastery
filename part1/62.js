c9edba965057689700892084dcc8d28f50f47861
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 00:58:36 +0530

    Update 62.js

--- part1/62.js
+++ part1/62.js
@@ -1,10 +1,10 @@
 // every method
-// const numbers = [2,4,6,9,10];
+const numbers = [2,4,6,9,10];
 
 
-// const ans = numbers.every((number)=>number%2===0);
+const ans = numbers.every((number)=>number%2===0);
 
-// console.log(ans);
+console.log(ans);
 
 const userCart = [
     {productId: 1, productName: "mobile", price: 12000},
