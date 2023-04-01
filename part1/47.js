d4e284c7546ac8e2634f66caaeb6b4c472556fa3
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 13:20:01 +0530

    Update 47.js

--- part1/47.js
+++ part1/47.js
@@ -1,10 +1,18 @@
 // hoisting 
+//hoisting happens only for  function definition
+//hoisting doesn't happen for function expression and arrow function it will give error
 
-// hello();
 
-// function hello(){
-//     console.log("hello world");
-// }
-// console.log(hello);
-// const hello = "hello world";
-// console.log(hello);
+hello();
+
+function hello(){
+    console.log("hello world");
+}
+console.log(hello); //error
+const hello = "hello world";
+console.log(hello);
+
+console.log(first); //undefined
+var first = "hello";
+
+console.log(first); //hello
