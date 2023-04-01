0e4cd58e938920869f3fce1c36a8fdf068fd3d26
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 15:54:06 +0530

    Update 57.js

--- part1/57.js
+++ part1/57.js
@@ -1,14 +1,15 @@
 // map method 
-// const numbers = [3,4,6,1,8];
 
-// const square = function(number){
-//     return number*number;
-// }
+const numbers = [3,4,6,1,8];
 
-// const squareNumber = numbers.map((number, index)=>{
-//     return index;
-// });
-// console.log(squareNumber);
+const square = function(number){
+    return number*number;
+}
+
+const squareNumber = numbers.map((number, index)=>{
+    return index;
+});
+console.log(squareNumber);
 
 const users = [
     {firstName: "harshit", age: 23},
