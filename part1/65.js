ab92bdb0e717b37c8a808cc0c5967d76113e234c
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 01:06:10 +0530

    Update 65.js

--- part1/65.js
+++ part1/65.js
@@ -4,10 +4,10 @@
 const myArray = ['item1', 'item2', 'item3'];
 
 // delete
-// const deletedItem = myArray.splice(1, 2);
-// console.log("delted item", deletedItem);
+const deletedItem = myArray.splice(1, 2);
+console.log("delted item", deletedItem);
 // insert 
-// myArray.splice(1, 0,'inserted item');
+myArray.splice(1, 0,'inserted item');
 
 // insert and delete 
 const deletedItem = myArray.splice(1, 2, "inserted item1", "inserted item2")
