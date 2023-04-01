d27fe647b537985eb35cc41c6d94a3381b389230
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-01 11:57:25 +0530

    Update 29.js

--- part1/29.js
+++ part1/29.js
@@ -3,9 +3,9 @@
 // how to concatenate two arrays
 
 let array1 = ["item1", "item2"];
-// let array2 = ["item1", "item2"];
-// let array2 = array1.slice(0).concat(["item3", "item4"]);
-// let array2 = [].concat(array1,["item3", "item4"]);
+let array2 = ["item1", "item2"];
+let array2 = array1.slice(0).concat(["item3", "item4"]); //this is fastedt way to create clone
+let array2 = [].concat(array1,["item3", "item4"]);
 // new way 
 // spread operator
 let oneMoreArray = ["item3", "item4"]
