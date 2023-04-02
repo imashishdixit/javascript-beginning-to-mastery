d48e8b3ec7c30a51817540d02549ca951704473c
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 12:44:02 +0530

    Update 75.js

--- part1/75.js
+++ part1/75.js
@@ -1,10 +1,11 @@
 // arrow functions 
+//arrow function takes this from as window which doesn't contain firstName and age that is why error will be thrown
 
 const user1 = {
     firstName : "harshit",
     age: 8,
     about: () => {
-        console.log(this.firstName, this.age);
+        console.log(this.firstName, this.age); //undefined undefined
     }   
 }
 
