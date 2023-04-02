68c80c8da240dff720ffae780df333205e16e987
Ashish Dixit <45693900+imashishdixit@users.noreply.github.com>
2023-04-02 12:38:00 +0530

    Update 73.js

--- part1/73.js
+++ part1/73.js
@@ -1,3 +1,5 @@
+
+
 function about(hobby, favMusician){
     console.log(this.firstName, this.age, hobby, favMusician);
 }
@@ -10,8 +12,10 @@ const user2 = {
     age: 9,
     
 }
-
+//call
+about.call(user1,"guitar","moazrt"); // call takes arugment as variable
 // apply
-// about.apply(user1, ["guitar", "bach"]);
-// const func = about.bind(user2, "guitar", "bach");
-// func();
+about.apply(user1, ["guitar", "bach"]); //apply takes value as an array
+//bind
+const func = about.bind(user2, "guitar", "bach"); //bind returns a function and we can call it later
+func();
